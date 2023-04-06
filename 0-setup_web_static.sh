bae07792ea017b95bc59622ad109dbbd7d608b18
bentalla108 <tallambaye108@gmail.com>
2023-04-06 15:21:30 -0700

    Script for server confguration

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,26 +1,48 @@
 #!/usr/bin/env bash
-# sets up web servers for the deployment of web_static:
-# - installs nginx
-# - creates /data/, /data/web_static/, /data/web_static/releases/,
-# - /data/web_static/releases/test, /data/web_static/releases/test/index.html
-# - creates a sym link /data/web_static/current -> /data/web_static/releases/test/
-# - grants /data/ -> ubuntu and group
-# - update nginx conf to serve /data/web_static/current/
-# - restart nginx
-apt-get -y update
-apt-get -y install nginx
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-web="<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>"
-echo "$web" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
-sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n}" /etc/nginx/sites-available/default
-service nginx restart
-exit 0
+# onfiguration de mon serveur  web_static
+
+echo -e "\e[1;33m START\e[0m"
+
+#--packages updating
+sudo apt-get -y update
+sudo apt-get -y install nginx
+echo -e "\e[1;32m Packages à jour\e[0m"
+echo
+
+#--firewall
+sudo ufw allow 'Nginx HTTP'
+echo -e "\e[1;33m Nginx installé \e[0m"
+echo
+
+#--test shared
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo -e "\e[1;32m Creation du dossier de test et partage"
+echo
+
+#--teststes
+echo "<h1>Welcome to bentalla.tech</h1>" > /data/web_static/releases/test/index.html
+echo -e "\e[1;32m Fichier de test\e[0m"
+echo
+
+#--Ecrasement au cas ou
+if [ -d "/data/web_static/current" ];
+then
+    echo "path /data/web_static/current exists"
+    sudo rm -rf /data/web_static/current;
+fi;
+echo -e "\e[1;32m prevent overwrite\e[0m"
+echo
+
+#--creation lien symbolic
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data
+
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
+sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
+echo -e "\e[1;32m Lien symbolic creer\e[0m"
+echo
+
+#--redemarrage NGINX
+sudo service nginx restart
+echo -e "\e[1;32m redemarrage de NGINX\e[0m"
